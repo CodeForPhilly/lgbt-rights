a5daa99e91883b9e6dbff319de66bc69b2aac1f0
Shawn Busolits <shawn.busolits@gmail.com>
2012-06-02 17:52:34 -0400

    Index properly redirects to the more_info page

--- public/javascripts/pgn.core.js
+++ public/javascripts/pgn.core.js
@@ -49,16 +49,15 @@ PGN.core = (function ($) {
             var id = value.id.split(':');
             var name = id[id.length - 1];
             list += '<li class="header">' + name + ' (' + key + ')<ul>';
-            value.rights.forEach(function (right) {
-	      if (right.link) {
-		list += '<li class="' + right.value + '"><a href="' + right.link + '" target="_blank">' + right.display_name + '</a>';
+            $.each(value.rights, function (key2, value) {
+	      if (value.more_info) {
+					list += '<li class="' + value.value + '"><a href="/more_info?key=' + key + '&right=' + key2 + '" target="_blank">' + value.display_name + '</a>';
 	      } else {
-		list += '<li class="' + right.value + '">' + right.display_name;
+					list += '<li class="' + value.value + '">' + value.display_name;
 	      }
 
-	      if (right.condition) {
-		//list += '<ul class="none"><li class="none">' + right.condition + '</li></ul>';
-		list += ' (' + right.condition + ')';
+	      if (value.condition) {
+					list += ' (' + value.condition + ')';
 	      }
 	      list += '</li>';
             });
